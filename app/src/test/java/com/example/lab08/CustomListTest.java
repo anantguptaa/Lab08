443272f298123ddc100b3df74f9d6d6d0759064f
anantguptaa <guptaanantt4@gmail.com>
2025-10-31 16:00:14 -0600

    added new test methods to test the countCities() feature

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -21,4 +21,27 @@ public class CustomListTest {
         list.deleteCity(edmonton);
         assertFalse(list.hasCity(edmonton));
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+
+        // Test with empty list
+        assertEquals(0, list.countCities());
+
+        // Add cities and test count
+        City calgary = new City("Toronto", "ON");
+        City edmonton = new City("Edmonton", "AB");
+        City vancouver = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
+
+        list.addCity(vancouver);
+        // This will fail initially because countCities() doesn't exist
+        assertEquals(3, list.countCities());
+    }
 }
